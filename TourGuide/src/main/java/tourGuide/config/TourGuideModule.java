44bca7236bc358ca52846eaac26d2e467068a525
Laura <laura_habdul@hotmail.fr>
2021-02-04 00:21:35 +0100

    Move TourGuideController to controller package and TourGuideModule to config package

---
+++ TourGuide/src/main/java/tourGuide/config/TourGuideModule.java
@@ -0,0 +1,35 @@
+package tourGuide.config;
+
+import gpsUtil.GpsUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rewardCentral.RewardCentral;
+import tourGuide.service.RewardsService;
+
+import java.util.Locale;
+
+@Configuration
+public class TourGuideModule {
+
+	@Bean
+	public Locale getLocale() {
+		Locale.setDefault(Locale.US);
+		return Locale.getDefault();
+	}
+	
+	@Bean
+	public GpsUtil getGpsUtil() {
+		return new GpsUtil();
+	}
+	
+	@Bean
+	public RewardsService getRewardsService() {
+		return new RewardsService(getGpsUtil(), getRewardCentral());
+	}
+	
+	@Bean
+	public RewardCentral getRewardCentral() {
+		return new RewardCentral();
+	}
+	
+}
