9b053e52a9f06c065c60aa72a1cc2918732bbf01
Laura <laura_habdul@hotmail.fr>
2021-03-03 01:41:36 +0100

    Moved RewardCentral.jar to libs package and add application.properties file

---
+++ rewards-microservice/src/main/java/RewardsApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(RewardsApplication.class, args);
+    }
+}
