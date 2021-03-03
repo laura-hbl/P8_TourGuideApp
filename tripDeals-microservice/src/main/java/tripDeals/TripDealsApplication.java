edce14d2bbc7c962980ea4c2c4d35826468f7f61
Laura <laura_habdul@hotmail.fr>
2021-03-03 02:42:31 +0100

    Creates dto classes

---
+++ tripDeals-microservice/src/main/java/tripDeals/TripDealsApplication.java
@@ -0,0 +1,13 @@
+package tripDeals;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripDealsApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TripDealsApplication.class, args);
+    }
+}
---
+++ tripDeals-microservice/src/main/java/tripDeals/dto/ProviderDTO.java
@@ -0,0 +1,18 @@
+package tripDeals.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class ProviderDTO {
+
+    private String name;
+
+    private double price;
+
+    private UUID tripId;
+}
