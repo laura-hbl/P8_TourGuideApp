c198b7bdc7bc81bf8c31e56f67ce2aad6216748e
Laura <laura_habdul@hotmail.fr>
2021-03-11 12:29:26 +0100

    Adds constant package and deletes TourGuideModule class

---
+++ TourGuide/src/main/java/tourGuide/constant/ProximityBuffer.java
@@ -0,0 +1,11 @@
+package tourGuide.constant;
+
+public class ProximityBuffer {
+
+    public static final int DEFAULT_PROXIMITY_BUFFER = 10;
+
+    public static final int ATTRACTION_PROXIMITY_RANGE = 200;
+
+    public ProximityBuffer() {
+    }
+}
---
+++ TourGuide/src/main/java/tourGuide/constant/UnitsConversion.java
@@ -0,0 +1,10 @@
+package tourGuide.constant;
+
+public class UnitsConversion {
+
+    public UnitsConversion() {
+    }
+
+    public static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
+
+}
