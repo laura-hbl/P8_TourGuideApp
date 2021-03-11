9f6a804e078dc0200d23ff30beb73e3351e099e8
Laura <laura_habdul@hotmail.fr>
2021-03-11 12:19:22 +0100

    Refactor InternalTestHelper and Tracker classes

--- TourGuide/src/main/java/tourGuide/helper/InternalTestHelper.java
+++ TourGuide/src/main/java/tourGuide/helper/InternalTestHelper.java
@@ -1,15 +1,83 @@
 package tourGuide.helper;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import tourGuide.model.Location;
+import tourGuide.model.VisitedLocation;
+import tourGuide.model.user.User;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
+import java.util.stream.IntStream;
+
+@Component
 public class InternalTestHelper {
 
-	// Set this default up to 100,000 for testing
-	private static int internalUserNumber = 100;
-	
-	public static void setInternalUserNumber(int internalUserNumber) {
-		InternalTestHelper.internalUserNumber = internalUserNumber;
+	private Logger logger = LoggerFactory.getLogger(InternalTestHelper.class);
+
+	public static final String TRIP_PRICER_API_KEY = "test-server-api-key";
+
+	private final Map<String, User> internalUserMap = new HashMap<>();
+
+	@Value("${test.user.numbers}")
+	private int internalUserNumber;
+
+	/**********************************************************************************
+	 *
+	 * Methods Below: For Internal Testing
+	 *
+	 **********************************************************************************/
+	// Database connection will be used for external users, but for testing purposes internal users are provided and
+	// stored in memory
+
+	public void initializeInternalUsers() {
+		IntStream.range(0, internalUserNumber).forEach(i -> {
+			String userName = "internalUser" + i;
+			String phone = "000";
+			String email = userName + "@tourGuide.com";
+			User user = new User(UUID.randomUUID(), userName, phone, email);
+			generateUserLocationHistory(user);
+
+			internalUserMap.put(userName, user);
+		});
+		logger.debug("Created " + internalUserNumber + " internal test users.");
 	}
-	
-	public static int getInternalUserNumber() {
-		return internalUserNumber;
+
+	private void generateUserLocationHistory(User user) {
+		IntStream.range(0, 3).forEach(i -> {
+			user.addToVisitedLocations(new VisitedLocation(user.getUserId(), new Location(generateRandomLatitude(),
+					generateRandomLongitude()), getRandomTime()));
+		});
+	}
+
+	private double generateRandomLongitude() {
+		double leftLimit = -180;
+		double rightLimit = 180;
+		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
+	}
+
+	private double generateRandomLatitude() {
+		double leftLimit = -85.05112878;
+		double rightLimit = 85.05112878;
+		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
+	}
+
+	private Date getRandomTime() {
+		LocalDateTime localDateTime = LocalDateTime.now().minusDays(new Random().nextInt(30));
+		return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
+	}
+
+	public static String getTripPricerApiKey() {
+		return TRIP_PRICER_API_KEY;
+	}
+
+	public Map<String, User> getInternalUserMap() {
+		return internalUserMap;
 	}
 }
