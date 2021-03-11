6a2e6291a33b5c8e67e2ede08d2f0fde0000d8c0
Laura <laura_habdul@hotmail.fr>
2021-03-11 12:56:11 +0100

    Improves calculateRewards feature and refactor RewardsService class

--- TourGuide/src/main/java/tourGuide/service/RewardsService.java
+++ TourGuide/src/main/java/tourGuide/service/RewardsService.java
@@ -1,80 +1,85 @@
 package tourGuide.service;
 
-import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tourGuide.constant.ProximityBuffer;
+import tourGuide.dto.AttractionDTO;
+import tourGuide.model.Location;
+import tourGuide.model.VisitedLocation;
+import tourGuide.model.user.User;
+import tourGuide.model.user.UserReward;
+import tourGuide.proxies.MicroserviceGpsProxy;
+import tourGuide.proxies.MicroserviceRewardsProxy;
+import tourGuide.util.DistanceCalculator;
+import tourGuide.util.ModelConverter;
 
-import gpsUtil.GpsUtil;
-import gpsUtil.location.Attraction;
-import gpsUtil.location.Location;
-import gpsUtil.location.VisitedLocation;
-import rewardCentral.RewardCentral;
-import tourGuide.user.User;
-import tourGuide.user.UserReward;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Service
-public class RewardsService {
-    private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
+public class RewardsService implements IRewardsService {
 
-	// proximity in miles
-    private int defaultProximityBuffer = 10;
-	private int proximityBuffer = defaultProximityBuffer;
-	private int attractionProximityRange = 200;
-	private final GpsUtil gpsUtil;
-	private final RewardCentral rewardsCentral;
-	
-	public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
-		this.gpsUtil = gpsUtil;
-		this.rewardsCentral = rewardCentral;
-	}
-	
-	public void setProximityBuffer(int proximityBuffer) {
-		this.proximityBuffer = proximityBuffer;
-	}
-	
-	public void setDefaultProximityBuffer() {
-		proximityBuffer = defaultProximityBuffer;
-	}
-	
-	public void calculateRewards(User user) {
-		List<VisitedLocation> userLocations = user.getVisitedLocations();
-		List<Attraction> attractions = gpsUtil.getAttractions();
-		
-		for(VisitedLocation visitedLocation : userLocations) {
-			for(Attraction attraction : attractions) {
-				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
-					if(nearAttraction(visitedLocation, attraction)) {
-						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-					}
-				}
-			}
-		}
-	}
-	
-	public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
-		return getDistance(attraction, location) > attractionProximityRange ? false : true;
-	}
-	
-	private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
-		return getDistance(attraction, visitedLocation.location) > proximityBuffer ? false : true;
-	}
-	
-	private int getRewardPoints(Attraction attraction, User user) {
-		return rewardsCentral.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
-	}
-	
-	public double getDistance(Location loc1, Location loc2) {
-        double lat1 = Math.toRadians(loc1.latitude);
-        double lon1 = Math.toRadians(loc1.longitude);
-        double lat2 = Math.toRadians(loc2.latitude);
-        double lon2 = Math.toRadians(loc2.longitude);
+    private Logger logger = LoggerFactory.getLogger(TourGuideService.class);
 
-        double angle = Math.acos(Math.sin(lat1) * Math.sin(lat2)
-                               + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+    private final ExecutorService executorService = Executors.newFixedThreadPool(400);
 
-        double nauticalMiles = 60 * Math.toDegrees(angle);
-        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
-        return statuteMiles;
-	}
+    private final MicroserviceGpsProxy gpsProxy;
 
+    private final MicroserviceRewardsProxy rewardsProxy;
+
+    private final ModelConverter modelConverter;
+
+    private final DistanceCalculator distanceCalculator;
+
+    @Autowired
+    public RewardsService(final MicroserviceGpsProxy gpsProxy, final MicroserviceRewardsProxy rewardsProxy,
+                          final ModelConverter modelConverter, final DistanceCalculator distanceCalculator) {
+        this.gpsProxy = gpsProxy;
+        this.rewardsProxy = rewardsProxy;
+        this.modelConverter = modelConverter;
+        this.distanceCalculator = distanceCalculator;
+    }
+
+    public void calculateRewards(final User user) {
+
+        CopyOnWriteArrayList<VisitedLocation> visitedLocation = new CopyOnWriteArrayList<>();
+        CopyOnWriteArrayList<AttractionDTO> attractions = new CopyOnWriteArrayList<>();
+
+        visitedLocation.addAll(user.getVisitedLocations());
+        attractions.addAll(gpsProxy.getAttractions());
+
+        visitedLocation.forEach(v -> {
+            attractions.stream()
+                    .filter(a -> isNearAttraction(v, a.getLocation()))
+                    .forEach(a -> {
+                        if (user.getUserRewards().stream()
+                                .noneMatch(r -> r.attraction.getAttractionName().equals(a.getAttractionName()))) {
+                            int rewardsPoint = rewardsProxy.getRewardPoints(a.getAttractionId(), user.getUserId());
+                            user.addUserReward(new UserReward(v, modelConverter.toAttraction(a), rewardsPoint));
+                        }
+                    });
+        });
+    }
+
+    public void calculateRewardsWithThreads(final User user) {
+        executorService.execute(new Runnable() {
+            public void run() {
+                calculateRewards(user);
+            }
+        });
+    }
+
+    private boolean isNearAttraction(final VisitedLocation visitedLocation, final Location attractionLocation) {
+        return !(distanceCalculator.getDistanceInMiles(attractionLocation, visitedLocation.getLocation()) >
+                ProximityBuffer.DEFAULT_PROXIMITY_BUFFER);
+    }
+
+    public void shutdown() throws InterruptedException {
+        executorService.shutdown();
+        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+    }
 }
