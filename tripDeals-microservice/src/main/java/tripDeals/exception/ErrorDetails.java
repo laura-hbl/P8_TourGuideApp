1ede7e0a6c38e345c15c3b2fb50af168d9572089
Laura <laura_habdul@hotmail.fr>
2021-05-03 22:32:05 +0200

    Adds GlobalExceptionHandler, ErrorDetails and custom exception class

---
+++ tripDeals-microservice/src/main/java/tripDeals/exception/ErrorDetails.java
@@ -0,0 +1,26 @@
+package tripDeals.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * Permits the storage and retrieving of customized error response.
+ *
+ * @author Laura Habdul
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private LocalDateTime timestamp;
+
+    private String message;
+
+    private String details;
+}
---
+++ tripDeals-microservice/src/main/java/tripDeals/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package tripDeals.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(final String message) {
+        super(message);
+    }
+}
