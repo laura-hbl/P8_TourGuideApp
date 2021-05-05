422fecc359e7d8e80f3337e90f55bd73e58e1c2b
Laura <laura_habdul@hotmail.fr>
2021-05-05 20:14:47 +0200

    Adds Constraints config class in Tourguide

---
+++ TourGuide/src/main/java/tourGuide/config/Constraints.java
@@ -0,0 +1,40 @@
+package tourGuide.config;
+
+/**
+ * Contains the different validator constraints.
+ *
+ * @author Laura Habdul
+ */
+public class Constraints {
+
+    /**
+     * Empty constructor of class Constraints.
+     */
+    public Constraints() {
+    }
+
+    /**
+     * Minimum number allowed for the user's travel preference for length of stay.
+     */
+    public static final int TRIP_DURATION_MIN_VALUE = 1;
+
+    /**
+     * Minimum number allowed for the user's travel preference for ticket quantity.
+     */
+    public static final int TICKET_QUANTITY_MIN_VALUE = 1;
+
+    /**
+     * Minimum value allowed the user's travel preference for the number of adults.
+     */
+    public static final int ADULT_NUMBER_MIN_VALUE = 1;
+
+    /**
+     * Minimum value allowed for the user's travel preference for the number of children.
+     */
+    public static final int CHILD_NUMBER_MIN_VALUE = 0;
+
+    /**
+     * Minimum value allowed for the user's preference on the lowest price limit.
+     */
+    public static final int LOWER_PRICE_VALUE = 0;
+}
--- TourGuide/src/main/java/tourGuide/dto/UserPreferencesDTO.java
+++ TourGuide/src/main/java/tourGuide/dto/UserPreferencesDTO.java
@@ -3,16 +3,17 @@ package tourGuide.dto;
 import javax.validation.constraints.Min;
 import lombok.*;
 import org.springframework.format.annotation.NumberFormat;
+import tourGuide.config.Constraints;
 
 /**
  * Permits the storage and retrieving data of an user preferences.
  *
  * @author Laura Habdul
  */
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
-@Getter(AccessLevel.PUBLIC)
-@Setter(AccessLevel.PUBLIC)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserPreferencesDTO {
 
     /**
@@ -24,7 +25,7 @@ public class UserPreferencesDTO {
     /**
      * The user's preference on the lowest price limit.
      */
-    @Min(value = 0, message = "Please enter a valid number")
+    @Min(value = Constraints.LOWER_PRICE_VALUE, message = "Please enter a valid number")
     private int lowerPricePoint;
 
     /**
@@ -35,23 +36,23 @@ public class UserPreferencesDTO {
     /**
      * The user's travel preference for length of stay.
      */
-    @Min(value = 1, message = "the length of stay must be at least 1")
+    @Min(value = Constraints.TRIP_DURATION_MIN_VALUE, message = "the length of stay must be at least 1")
     private int tripDuration;
 
     /**
      * The user's travel preference for ticket quantity.
      */
-    @Min(value = 1, message = "the number of tickets must be at least 1")
+    @Min(value = Constraints.TICKET_QUANTITY_MIN_VALUE, message = "the number of tickets must be at least 1")
     private int ticketQuantity;
 
     /**
      * The user's travel preference for the number of adults.
      */
-    @Min(value = 1, message = "the number of adult must be at least 1")
+    @Min(value = Constraints.ADULT_NUMBER_MIN_VALUE, message = "the number of adult must be at least 1")
     private int numberOfAdults;
     /**
      * The user's travel preference for the number of children.
      */
-    @Min(value = 0, message = "Please enter a valid number")
+    @Min(value = Constraints.CHILD_NUMBER_MIN_VALUE, message = "Please enter a valid number")
     private int numberOfChildren;
 }
